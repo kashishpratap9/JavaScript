e348b10a53ab23bd8d9b97ed7ff5e01d0cae5a4d
kashishpratap9 <kashishpratap4@gmail.com>
2025-06-12 19:07:50 +0000

    push

---
+++ Basic/var.js
@@ -0,0 +1,6 @@
+const accountId= 134252
+let accountMail= "kashish@google.com"
+var accountName= "rcb123425"
+accountLocation = "jaipur"
+
+console.table([accountId, accountMail, accountName, accountLocation])
